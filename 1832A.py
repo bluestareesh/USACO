79b4047f170652f92af5b0a3c9cde9a075365abd
bluestareesh <bluestareesh@gmail.com>
2023-08-10 13:58:06 -0500

    Organ

--- 1832A.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-  s = list(input())
-  temp = []
-  for i in range(len(s)//2):
-    temp.append(s[i])
-  if len(set(temp)) == 1:
-    print('NO')
-  else:
-    print('YES')
--- 520A.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-if len(set(input().lower())) == 26:
-  print('YES')
-else:
-  print('NO')
