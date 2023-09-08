554220eb9e01531a7dc47e70da9af5203765b72a
bluestareesh <bluestareesh@gmail.com>
2023-09-07 22:54:56 -0500

    problem solved

--- USACO 2023 Open problem 1.cpp
+++ USACO 2023 Open problem 1.cpp
@@ -5,49 +5,55 @@
 #include <stdio.h>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <string>
 using namespace std;
 typedef long long ll;
 # define int ll
 int32_t main(void) {
     int n;
     cin >> n;
-    vector <int> all;
-    int su = 0;
-    vector <int> green(n+1);
-    int total = 0;
-    for (int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        all.push_back(temp);
-    }
-    vector<int> rem(all);
-    sort(all.begin(),all.end());
-    for (int i = n-1; i >= 0; i--) {
-        if (i < n-1) {
-            green[i] = green[i+1];
+    vector<pair<int,pair<string,int>>> x;
+    for (int i =0 ;i < n; i++ ){
+        int temp,val;
+        string name;
+        char d;
+        cin >> temp >> name >> d >> val;
+        if (d == '-') {
+            val = -val;
         }
-        green[i] += all[i];
-        total += green[i];
+        x.push_back({temp,{name,val}});
     }
-    int q;
-    cin >> q;
-    for (int i = 0; i < q; i++) {
-        int temp,temp2;
-        cin >> temp >> temp2;
-        temp--;
-        int pos = lower_bound(all.begin(),all.end(),temp2) - all.begin();
-        int old_pos = lower_bound(all.begin(),all.end(),rem[temp]) - all.begin();
-        int fina = total;
-        if (pos <= old_pos) {
-            fina += green[pos] - green[old_pos];
-            fina -= (old_pos + 1) * rem[temp];
-            fina += (pos + 1) * temp2;
+    sort(x.begin(),x.end());
+    vector<int> cows = {7,7,7};
+    vector<string> cur = {"Bessie","Mildred","Elsie"};
+    int answer = 0;
+    for (auto i : x) {
+        if (i.second.first == "Bessie") {
+            cows[0] += i.second.second;
         }
-        else {
-            fina -= green[old_pos+1] - green[pos];
-            fina -= (old_pos + 1) * rem[temp];
-            fina += (pos) * temp2;
+        if (i.second.first == "Mildred") {
+            cows[1] += i.second.second;
         }
-        cout << fina << '\n';
+        if (i.second.first == "Elsie") {
+            cows[2] += i.second.second;
+        }
+        vector<string> cur2;
+        int temp = *max_element(cows.begin(),cows.end());
+        if (cows[0] == temp ){
+            cur2.push_back("Bessie");
+        } 
+        if (cows[1] == temp) {
+            cur2.push_back("Mildred");
+        }
+        if (cows[2] == temp) {
+            cur2.push_back("Elsie");
+        }
+        if (cur2 != cur) {
+            answer += 1;
+            cur = cur2;
+        }
     }
+    cout << answer << '\n';
 }
